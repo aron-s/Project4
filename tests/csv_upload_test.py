2593475ddfbed282267f14a8b0c9e508b1016dea
aron-s <as4259@njit.edu>
2022-05-11 23:44:02 -0400

    error handlers

--- tests/csv_upload_test.py
+++ tests/csv_upload_test.py
@@ -3,8 +3,8 @@ import os
 
 def test_uploads():
     """test uploads"""
-    assert os.path.exists('./app/uploads/music.csv') == True
+    assert os.path.exists('./app/uploads/') == True
 
 def test_database():
     """test database once csv is processed"""
-    assert os.path.exists('./database/db2.sqlite') == True
+    assert os.path.exists('./database/') == True
