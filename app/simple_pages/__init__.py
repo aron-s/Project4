f5d3468090dd54a48f178da2607bd9710400b4e8
aron-s <as4259@njit.edu>
2022-05-11 23:19:17 -0400

    added simple pages

---
+++ app/simple_pages/__init__.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+from flask import logging
+
+simple_pages = Blueprint('simple_pages', __name__,
+                        template_folder='templates')
+
+
+@simple_pages.route('/')
+def index():
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
+
+@simple_pages.route('/about')
+def about():
+    try:
+        return render_template('about.html')
+    except TemplateNotFound:
+        abort(404)
+
+@simple_pages.route('/welcome')
+def welcome():
+    try:
+        return render_template('welcome.html')
+    except TemplateNotFound:
+        abort(404)
+
+# if __name__ == '__main__':
+#     log = logging.getLogger("__main__")
+#     log.DEBUG('method:{}, route:{}, time:{}, request address{}')
